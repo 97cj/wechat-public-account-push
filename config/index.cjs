6667f6afadd3888cb3255313a55b37e6633aca6f
97cj <127743440+97cj@users.noreply.github.com>
2023-03-13 20:30:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o0XKh6qwgRc6YNJCUNDGC3Un6baw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'idNJDpAQQElWDjziimcCRhD3Pg2P8FxDc4pGb4fzSMg',
+      useTemplateId: 'gl1CzQ8nH7bvT-58LhGojEv4-Zuuf4TPiU9dVHgB-9Q',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-31',
       festivals: [
